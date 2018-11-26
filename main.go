a158f7d78475e6d85306fa26fa211df42eefb7a3
unknown <luiyicpu@hotmail.com>
2018-11-26 19:59:20 -0400

    Added get document by id method

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ type Document struct {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/documents", getDocuments).Methods("GET")
+	router.HandleFunc("/documents/{id}", getDocumentById).Methods("GET")
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
 
@@ -48,4 +49,26 @@ func getMD5Checksum(content string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(content))
 	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func getDocumentById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	id := vars["id"]
+	path := "./files/"
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		b, err := ioutil.ReadFile(path + f.Name())
+		if err != nil {
+			fmt.Print(err)
+		}
+		fileContent := string(b)
+		if getMD5Checksum(fileContent) == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(Document{ID: getMD5Checksum(fileContent), Name: f.Name(), Size: f.Size()})
+		}
+	}
 }
